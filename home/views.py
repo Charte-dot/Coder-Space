1069f8b15d7df5f9b9927d425697ba97b8218c87
Caroline Harte <caroline.harte@yahoo.com>
2022-12-03 19:21:19 +0000

    Added about page, sitemap, robot txt and privacy file

--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     """View to return index page"""
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to render the about page """
+    return render(request, 'home/about.html')
+
+
+def privacy(request):
+    """ A view to render the privacy policy page """
+    return render(request, 'home/privacy.html')
